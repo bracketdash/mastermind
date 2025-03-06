83ae9fe8ca78ab2c2c3999c56988359f64a47f51
Michael Hatch <michaelhatch@me.com>
2025-03-05 19:45:54 -0800

    refactor game logic

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // TODO: UI interactions will go here
+
+// Example run:
+
+const history = [
+  ["abcd", 1, 2], // First guess "abcd" had 1 red peg and 2 white pegs
+  ["efba", 0, 3], // Second guess "efba" had 0 red pegs and 3 white pegs
+  ["cdfe", 2, 0], // Third guess "cdfe" had 2 red pegs and 0 white pegs
+];
+
+const guess = "effe";
+
+console.log("Spinner starts spinning...");
+console.time("Total spin time");
+getFeedback(history, guess).then((pegs) => {
+  console.log(JSON.stringify(pegs));
+  console.timeEnd("Total spin time");
+});
--- game.js
+++ game.js
@@ -1,26 +1,29 @@
+const chars = "abcdef";
 const fitsCluesCache = {};
 const getHistorySliceCache = {};
 const mapCombosCache = {};
 
 function fitsClues(history, combo, historyStr) {
-  if (fitsCluesCache[historyStr + combo]) {
-    return fitsCluesCache[historyStr + combo];
+  const cacheKey = historyStr + combo;
+  if (fitsCluesCache[cacheKey] !== undefined) {
+    return fitsCluesCache[cacheKey];
   }
   let passing = true;
-  history.forEach((slice) => {
+  for (let i = 0; i < history.length; i++) {
+    const slice = history[i];
     if (combo === slice[0]) {
       passing = false;
-      return;
+      break;
     }
     if (!slice[1]) {
-      slice[0].split("").some((char, index) => {
-        if (char === combo[index]) {
+      for (let j = 0; j < slice[0].length; j++) {
+        if (slice[0][j] === combo[j]) {
           passing = false;
-          return true;
+          break;
         }
-      });
+      }
       if (!passing) {
-        return;
+        break;
       }
     }
     const pegs = slice[1] + slice[2];
@@ -29,15 +32,17 @@ function fitsClues(history, combo, historyStr) {
       .filter((c) => slice[0].includes(c)).length;
     if ((!pegs && numSameChars) || (pegs && numSameChars < pegs)) {
       passing = false;
+      break;
     }
-  });
-  fitsCluesCache[historyStr + combo] = passing;
+  }
+  fitsCluesCache[cacheKey] = passing;
   return passing;
 }
 
 function getHistorySlice(combo, guess) {
-  if (getHistorySliceCache[combo + guess]) {
-    return getHistorySliceCache[combo + guess];
+  const cacheKey = combo + guess;
+  if (getHistorySliceCache[cacheKey] !== undefined) {
+    return getHistorySliceCache[cacheKey];
   }
   let red = 0;
   let white = 0;
@@ -49,16 +54,14 @@ function getHistorySlice(combo, guess) {
     }
   });
   const slice = [combo, red, white];
-  getHistorySliceCache[combo + guess] = slice;
+  getHistorySliceCache[cacheKey] = slice;
   return slice;
 }
 
 function mapCombos(history, callback) {
-  const chars = "abcdef";
   const possibleCombos = [];
-
   const historyStr = history.map((s) => s.join("")).join("");
-  if (!callback && mapCombosCache[historyStr]) {
+  if (!callback && mapCombosCache[historyStr] !== undefined) {
     return mapCombosCache[historyStr];
   }
   const recursor = (combo) => {
@@ -79,35 +82,30 @@ function mapCombos(history, callback) {
   return possibleCombos;
 }
 
-// TODO: adapt to return data instead of console.logging it
 function getFeedback(history, guess) {
-  const combos = mapCombos(history);
-  const numCombos = combos.length;
-  const codes = combos.map((code, index) => {
-    let total = 0;
-    combos.forEach((combo) => {
-      const numCombosAfter = mapCombos([
-        ...history,
-        getHistorySlice(combo, code),
-      ]).length;
-      total += numCombos - numCombosAfter;
-    });
-    process.stdout.clearLine(0);
-    process.stdout.cursorTo(0);
-    process.stdout.write(
-      `Calculating feedback... ${((index * 100) / numCombos).toFixed(2)}%`
-    );
-    return {
-      slice: getHistorySlice(code, guess),
-      avg: total / numCombos,
-    };
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const combos = mapCombos(history);
+      const numCombos = combos.length;
+      const codes = combos.map((code) => {
+        let total = 0;
+        combos.forEach((combo) => {
+          const numCombosAfter = mapCombos([
+            ...history,
+            getHistorySlice(combo, code),
+          ]).length;
+          total += numCombos - numCombosAfter;
+        });
+        return {
+          slice: getHistorySlice(code, guess),
+          avg: total / numCombos,
+        };
+      });
+      codes.sort((a, b) => (a.avg > b.avg ? 1 : a.avg < b.avg ? -1 : 0));
+      resolve({
+        red: codes[0].slice[1],
+        white: codes[0].slice[2],
+      });
+    }, 0);
   });
-  codes.sort((a, b) => (a.avg > b.avg ? 1 : a.avg < b.avg ? -1 : 0));
-  process.stdout.clearLine(0);
-  process.stdout.cursorTo(0);
-  console.log(
-    [...history, [guess, codes[0].slice[1], codes[0].slice[2]]]
-      .map((s) => s.join(":"))
-      .join(",")
-  );
 }
