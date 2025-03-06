a57036af501cbc91d95b28675611fac53e585da1
Michael Hatch <michaelhatch@me.com>
2025-03-06 07:29:46 -0800

    start on scripting the ui

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-// TODO: UI interactions will go here
+const msgThinking = document.getElementById("thinking");
+const controls = document.querySelectorAll(".controls > .column > div");
+const btnSubmit = document.getElementById("btn-submit");
+const btnUndo = document.getElementById("btn-undo");
 
-// Example run:
-
-const history = [
-  ["abcd", 1, 2], // First guess "abcd" had 1 red peg and 2 white pegs
-  ["efba", 0, 3], // Second guess "efba" had 0 red pegs and 3 white pegs
-  ["cdfe", 2, 0], // Third guess "cdfe" had 2 red pegs and 0 white pegs
-];
-
-const guess = "effe";
-
-console.log("Spinner starts spinning...");
-console.time("Total spin time");
-getFeedback(history, guess).then((pegs) => {
-  console.log(JSON.stringify(pegs));
-  console.timeEnd("Total spin time");
-});
+function handleSubmitClick() {
+  msgThinking.classList.remove("hidden");
+  // TODO: get the actual history from each history-row
+  const history = [
+    ["abcd", 1, 2], // First guess "abcd" had 1 red peg and 2 white pegs
+    ["efba", 0, 3], // Second guess "efba" had 0 red pegs and 3 white pegs
+    ["cdfe", 2, 0], // Third guess "cdfe" had 2 red pegs and 0 white pegs
+  ];
+  // TODO: get the user's actual guess from controls selected classes
+  const guess = "effe";
+  getFeedback(history, guess).then((pegs) => {
+    msgThinking.classList.add("hidden");
+    // TODO: add a new history-row (removing .initial if still present)
+  });
+}
